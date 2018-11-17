17d165f3e2e3f6389621ed3b23995420886a993c
root <root@mr-aws.internal.virtnet>
2018-11-17 17:54:11 -0500

    we gon get it

--- myproject.py
+++ myproject.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello There! What's good with you, keep goin fam</h1>"
+    return "<h1 style='color:blue'>Hello There! What's good with you, keep goin fam. We gon get it</h1>"
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
