d90089a1d8faaf8ec1bae9c5d3404d2d70b2b226
root <root@mr-aws.internal.virtnet>
2018-11-18 18:48:07 -0500

    changed myproject.py to invoke codepipeline

--- myproject.py
+++ myproject.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello there!  Keep on learning and continue to get better.</h1>"
+    return "<h1 style='color:blue'>Hello there!  Keep on learning and continue to get better.  11/18/2018 6:47 PM.</h1>"
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
