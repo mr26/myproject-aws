4ea0cae7412548479d605d1330c3d42de666bf51
root <root@mr-aws.internal.virtnet>
2018-11-18 16:41:54 -0500

    changed myproject.py

--- myproject.py
+++ myproject.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello There! What's good with you, keep goin fam. We gon get it. Strive and propser.</h1>"
+    return "<h1 style='color:blue'>Hello there!  Keep on learning and continue to get better.</h1>"
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
